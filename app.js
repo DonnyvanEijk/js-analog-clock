884d80d3429a7cb13990d34ab417dcddb062778c
DonnyvanEijk <donnyvaneijk873@gmail.com>
2025-12-03 22:42:35 +0100

    Klok bugfix calculation, also differantiate hands

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const hourHand = document.querySelector('.hour-hand');
 
 function setDate() {
     const now = new Date();
+
     const seconds = now.getSeconds();
     const secondsDegrees = ((seconds / 60) * 360) + 90;
     secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
@@ -13,9 +14,9 @@ function setDate() {
     minsHand.style.transform = `rotate(${minsDegrees}deg)`;
 
     const hour = now.getHours();
-    const hourDegrees = ((hour / 12) * 360) + 90;
+    const hourDegrees = ((hour / 12) * 360) + ((mins / 60) * 30) + 90;
     hourHand.style.transform = `rotate(${hourDegrees}deg)`;
-
 }
 
 setInterval(setDate, 1000);
+setDate();
